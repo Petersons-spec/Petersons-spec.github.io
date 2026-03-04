205a213f995f82ae0b0bf2cc38e93b8cc93c310f
Petersons-spec <petersons1983@gmail.com>
2026-03-04 14:57:43 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBT_mqJrv4I3sxqQdHIzh3dpHoIrRLGhwg",
+  authDomain: "fands-5116d.firebaseapp.com",
+  projectId: "fands-5116d",
+  storageBucket: "fands-5116d.firebasestorage.app",
+  messagingSenderId: "742576835870",
+  appId: "1:742576835870:web:64eb7665b664c27e7f0d0f",
+  measurementId: "G-F0SLMHNEFX"
+};
